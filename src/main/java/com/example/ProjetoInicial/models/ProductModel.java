47455cd5a356a0f47df90be9f7460757bce1e5bd
MatheusCosta616 <rm551157@fiap.com.br>
2024-01-04 22:25:32 -0300

    projeto v1!

---
+++ src/main/java/com/example/ProjetoInicial/models/ProductModel.java
@@ -0,0 +1,31 @@
+package com.example.ProjetoInicial.models;
+
+
+import jakarta.persistence.*;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PRODUCTS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductModel extends RepresentationModel<ProductModel> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
+
+}
---
+++ src/main/java/com/example/ProjetoInicial/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.example.ProjetoInicial.repositories;
+
+import com.example.ProjetoInicial.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
+}
